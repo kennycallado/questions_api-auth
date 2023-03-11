02129e9e4e631a936e605ec3af2c30658fb5007c
kennycallado <kennycallado@hotmail.com>
2023-03-11 17:25:16 +0100

    ADD Agrega script para la compilación y creación de imágenes de docker

---
+++ build.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# This script compiles the binary using the rust-musl-builder docker image,
+# and build the docker images for multiples platforms.
+#
+# In order to make the images to work, you need to install
+# qemu-user-static in the host machine, because the binary
+# is compiled for a different architecture.
+
+platforms=("linux/amd64" "linux/arm64" "linux/arm/v7" "linux/arm/v6")
+
+package_name=$(cat Cargo.toml | grep 'name' | awk '{print $3}' | tr -d '"')
+version=$(cat Cargo.toml | grep 'version' | head -1 | awk '{print $3}' | tr -d '"')
+
+# Permissions for target folder
+chmod -R o+w target
+
+# Build the binary
+docker run --rm -it -v "$(pwd)":/home/rust/src ekidd/rust-musl-builder cargo build --release
+
+for platform in ${platforms[@]}; do
+  tag=$(echo "${platform//\//_}" | tr -d 'linux_' | xargs -I {} echo {})
+  echo "Building docker image for: $platform."
+
+  docker build --no-cache --pull \
+    --platform ${platform} \
+    -t kennycallado/${package_name}:${version}-${tag} \
+    --build-arg PACKAGE_NAME=${package_name} \
+    -f ./Containerfile .
+done
+
+# push the images
+docker push -a kennycallado/${package_name}
+
+# create the manifest
+docker manifest create kennycallado/${package_name}:${version} \
+  --amend kennycallado/${package_name}:${version}-amd64 \
+  --amend kennycallado/${package_name}:${version}-arm64 \
+  --amend kennycallado/${package_name}:${version}-armv7 \
+  --amend kennycallado/${package_name}:${version}-armv6
+
+# manifest for latest version
+docker manifest create kennycallado/${package_name}:latest \
+  --amend kennycallado/${package_name}:${version}-amd64 \
+  --amend kennycallado/${package_name}:${version}-arm64 \
+  --amend kennycallado/${package_name}:${version}-armv7 \
+  --amend kennycallado/${package_name}:${version}-armv6
+
+# push the manifests
+docker manifest push kennycallado/${package_name}:${version}
+docker manifest push kennycallado/${package_name}:latest
+
+# tag the latest version
+docker tag kennycallado/${package_name}:${version} kennycallado/${package_name}:latest
+docker push kennycallado/${package_name}:latest
+
+# remove the images
+docker rmi kennycallado/${package_name}:${version}-amd64
+docker rmi kennycallado/${package_name}:${version}-arm64
+docker rmi kennycallado/${package_name}:${version}-armv7
+docker rmi kennycallado/${package_name}:${version}-armv6
+
+docker system prune -f
