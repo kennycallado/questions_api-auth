8d9e81dae1e755b280163c2653336df155f09045
kennycallado <kennycallado@hotmail.com>
2023-03-14 12:01:52 +0100

    UPDATE Auth devuelve userinclaims con el token

--- src/app/modules/auth/controller.rs
+++ src/app/modules/auth/controller.rs
@@ -1,24 +1,33 @@
 use rocket::http::{Cookie, CookieJar, Status};
 use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
 
 use crate::app::providers::guards::claims::RefreshClaims;
 
 use crate::app::modules::auth::services::helpers;
+use crate::app::providers::interfaces::helpers::claims::UserInClaims;
 
 pub fn routes() -> Vec<rocket::Route> {
     routes![auth_bypass, auth, login_options, login]
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct AuthUser {
+    pub id: UserInClaims,
+    pub access_token: String,
+}
+
 // WARNING: This is only for testing purposes
 #[get("/bypass/<id>")]
-pub async fn auth_bypass(cookie: &CookieJar<'_>, id: i32) -> Result<Json<String>, Status> {
+pub async fn auth_bypass(cookie: &CookieJar<'_>, id: i32) -> Result<Json<AuthUser>, Status> {
     let user_in_claims = helpers::user_request(id).await;
     if let Err(_) = user_in_claims {
         return Err(Status::InternalServerError);
     }
     let user_in_claims = user_in_claims.unwrap();
 
-    let tokens = helpers::token_generator(user_in_claims).await;
+    let tokens = helpers::token_generator(user_in_claims.clone()).await;
 
     if let Err(_) = tokens {
         return Err(Status::NotFound);
@@ -27,27 +36,31 @@ pub async fn auth_bypass(cookie: &CookieJar<'_>, id: i32) -> Result<Json<String>
 
     cookie.add_private(Cookie::new("refresh_token", refresh_token));
 
-    Ok(Json(access_token))
+    let auth_user = AuthUser {
+        id: user_in_claims,
+        access_token,
+    };
+    Ok(Json(auth_user))
 }
 
 #[get("/")]
-pub async fn auth(cookie: &CookieJar<'_>, claims: RefreshClaims) -> Result<Json<String>, Status> {
+pub async fn auth(cookie: &CookieJar<'_>, claims: RefreshClaims) -> Result<Json<AuthUser>, Status> {
     let user_in_claims = helpers::user_request(claims.0.user.id).await;
     if let Err(_) = user_in_claims {
         return Err(Status::InternalServerError);
     }
     let user_in_claims = user_in_claims.unwrap();
 
-    // En este punto son diferentes
-    // if user_in_claims.user_token != claims.user.user_token {
-    //     return Err(Status::Unauthorized);
-    // }
-
-    match helpers::token_generator(user_in_claims).await {
+    match helpers::token_generator(user_in_claims.clone()).await {
         Ok((refresh_token, access_token)) => {
             cookie.add_private(Cookie::new("refresh_token", refresh_token));
 
-            Ok(Json(access_token))
+            let auth_user = AuthUser {
+                id: user_in_claims,
+                access_token,
+            };
+
+            Ok(Json(auth_user))
         }
         Err(e) => {
             return Err(e);
@@ -62,7 +75,7 @@ pub async fn login_options() -> Status {
 }
 
 #[post("/login", data = "<token>")]
-pub async fn login(cookie: &CookieJar<'_>, token: String) -> Result<Json<String>, Status> {
+pub async fn login(cookie: &CookieJar<'_>, token: String) -> Result<Json<AuthUser>, Status> {
     // Request the user_id from the profile api
     let response = helpers::profile_request(token).await;
     if let Err(e) = response {
@@ -76,11 +89,16 @@ pub async fn login(cookie: &CookieJar<'_>, token: String) -> Result<Json<String>
     }
     let user_in_claims = user_in_claims.unwrap();
 
-    match helpers::token_generator(user_in_claims).await {
+    match helpers::token_generator(user_in_claims.clone()).await {
         Ok((refresh_token, access_token)) => {
             cookie.add_private(Cookie::new("refresh_token", refresh_token.clone()));
 
-            Ok(Json(access_token))
+            let auth_user = AuthUser {
+                id: user_in_claims,
+                access_token,
+            };
+
+            Ok(Json(auth_user))
         }
         Err(e) => {
             return Err(e);
