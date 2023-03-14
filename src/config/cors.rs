50563b944358548b856eaaaea16ede3f4f192663
kennycallado <kennycallado@hotmail.com>
2023-03-14 12:40:56 +0100

    FIX Usa parámetro de configuración origin_url

--- src/config/cors.rs
+++ src/config/cors.rs
@@ -2,6 +2,8 @@ use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
 use rocket::{Request, Response};
 
+use crate::app::providers::interfaces::helpers::config_getter::ConfigGetter;
+
 pub struct Cors;
 
 #[rocket::async_trait]
@@ -14,7 +16,9 @@ impl Fairing for Cors {
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
-        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        let origin = ConfigGetter::get_origin_url().unwrap_or("*".to_string());
+         
+        response.set_header(Header::new("Access-Control-Allow-Origin", origin));
         response.set_header(Header::new(
             "Access-Control-Allow-Methods",
             "POST, GET, PUT, OPTIONS",
