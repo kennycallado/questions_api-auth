070ed99e3cdc06e3d5f53415290e5c941a656113
kennycallado <kennycallado@hotmail.com>
2023-03-30 11:48:10 +0200

    BUMP Update dependencias and manage new migrations_run implementation

--- src/config/database.rs
+++ src/config/database.rs
@@ -7,16 +7,19 @@ use crate::app::providers::interfaces::helpers::config_getter::ConfigGetter;
 pub struct Db(diesel::PgConnection);
 
 pub async fn run_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
-    embed_migrations!("src/database/migrations");
+    use diesel_migrations::{EmbeddedMigrations, MigrationHarness};
+
+    const MIGRATIONS: EmbeddedMigrations = embed_migrations!("src/database/migrations");
 
     if ConfigGetter::get_migrations_run().unwrap_or(true) {
-        let conn = Db::get_one(&rocket)
+        Db::get_one(&rocket)
             .await
-            .expect("Error: run_migrations; database connection");
-
-        conn.run(|conn| embedded_migrations::run(conn))
-            .await
-            .expect("Error: run_migrations; running migrations");
+            .expect("ERROR: database.run_migrations(); database connection")
+            .run(|conn| {
+                conn.run_pending_migrations(MIGRATIONS)
+                    .expect("ERROR: database.run_migrations(); diesel migrations");
+            })
+            .await;
     }
 
     rocket
